e33a2e57b1a79ac2eb5c75b4060c4ad0aeaa77ad
Niko <nikooin@gmail.com>
2024-12-04 19:43:33 +0200

    Day 4

--- src/days/day_04.zig
+++ src/days/day_04.zig
@@ -4,18 +4,135 @@ const utils = @import("../utils.zig");
 const stdout = std.io.getStdOut().writer();
 const test_allocator = std.testing.allocator;
 
+const Position = struct { i: i32, j: i32 };
+
 pub fn part1(allocator: std.mem.Allocator, input: []const u8) !u32 {
-    _ = allocator;
-    _ = input;
-    return 0;
+    const table = try utils.Matrix(u8).initFromString(allocator, input);
+    defer table.deinit();
+
+    var num_xmas: u32 = 0;
+    for (0..table.n_rows) |i| {
+        for (0..table.n_cols) |j| {
+            if (table.get(i, j) != 'X') {
+                continue;
+            }
+            var neighbours: []const Position = &.{
+                .{ .i = -1, .j = 0 },
+                .{ .i = 0, .j = -1 },
+                .{ .i = 1, .j = 0 },
+                .{ .i = 0, .j = 1 },
+                .{ .i = -1, .j = -1 },
+                .{ .i = 1, .j = -1 },
+                .{ .i = -1, .j = 1 },
+                .{ .i = 1, .j = 1 },
+            };
+            var ind: usize = 1;
+            while (neighbours.len > 0 and ind < 4) {
+                var new_neighbours = std.ArrayList(Position).init(allocator);
+                defer new_neighbours.deinit();
+                for (neighbours) |offset| {
+                    const i_new: i32 = @as(i32, @intCast(i)) + @as(i32, @intCast(ind)) * offset.i;
+                    const j_new: i32 = @as(i32, @intCast(j)) + @as(i32, @intCast(ind)) * offset.j;
+                    if (i_new < 0 or i_new >= table.n_rows or j_new < 0 or j_new >= table.n_cols) {
+                        continue;
+                    }
+                    if (table.get(@as(usize, @intCast(i_new)), @as(usize, @intCast(j_new))) == "XMAS"[ind]) {
+                        try new_neighbours.append(offset);
+                    }
+                }
+                if (ind > 1) {
+                    allocator.free(neighbours);
+                }
+                neighbours = try new_neighbours.toOwnedSlice();
+                ind += 1;
+            }
+            num_xmas += @truncate(neighbours.len);
+            allocator.free(neighbours);
+        }
+    }
+
+    return num_xmas;
 }
 
 pub fn part2(allocator: std.mem.Allocator, input: []const u8) !u32 {
-    _ = allocator;
-    _ = input;
-    return 0;
+    const table = try utils.Matrix(u8).initFromString(allocator, input);
+    defer table.deinit();
+
+    var mas_list = std.ArrayList(Position).init(allocator);
+    defer mas_list.deinit();
+    for (0..table.n_rows) |i| {
+        for (0..table.n_cols) |j| {
+            if (table.get(i, j) != 'M') {
+                continue;
+            }
+            var neighbours: []const Position = &.{
+                .{ .i = -1, .j = -1 },
+                .{ .i = 1, .j = -1 },
+                .{ .i = -1, .j = 1 },
+                .{ .i = 1, .j = 1 },
+            };
+            var ind: usize = 1;
+            while (neighbours.len > 0 and ind < 3) {
+                var new_neighbours = std.ArrayList(Position).init(allocator);
+                defer new_neighbours.deinit();
+                for (neighbours) |offset| {
+                    const i_new: i32 = @as(i32, @intCast(i)) + @as(i32, @intCast(ind)) * offset.i;
+                    const j_new: i32 = @as(i32, @intCast(j)) + @as(i32, @intCast(ind)) * offset.j;
+                    if (i_new < 0 or i_new >= table.n_rows or j_new < 0 or j_new >= table.n_cols) {
+                        continue;
+                    }
+                    if (table.get(@as(usize, @intCast(i_new)), @as(usize, @intCast(j_new))) == "MAS"[ind]) {
+                        try new_neighbours.append(offset);
+                    }
+                }
+                if (ind > 1) {
+                    allocator.free(neighbours);
+                }
+                neighbours = try new_neighbours.toOwnedSlice();
+                ind += 1;
+            }
+            for (neighbours) |offset| {
+                const i_middle: usize = @intCast(@as(i32, @intCast(i)) + offset.i);
+                const j_middle: usize = @intCast(@as(i32, @intCast(j)) + offset.j);
+                try mas_list.append(.{ .i = @intCast(@as(i32, @intCast(i_middle))), .j = @intCast(@as(i32, @intCast(j_middle))) });
+            }
+            allocator.free(neighbours);
+        }
+    }
+
+    var num_xmas: u32 = 0;
+    for (0..mas_list.items.len - 1) |i| {
+        for (i + 1..mas_list.items.len) |j| {
+            const mas1 = mas_list.items[i];
+            const mas2 = mas_list.items[j];
+            if (mas1.i == mas2.i and mas1.j == mas2.j) {
+                num_xmas += 1;
+            }
+        }
+    }
+
+    return num_xmas;
 }
 
-test "day04_part1" {}
+const test_input =
+    \\MMMSXXMASM
+    \\MSAMXMSMSA
+    \\AMXSXMAAMM
+    \\MSAMASMSMX
+    \\XMASAMXAMM
+    \\XXAMMXXAMA
+    \\SMSMSASXSS
+    \\SAXAMASAAA
+    \\MAMMMXMMMM
+    \\MXMXAXMASX
+;
 
-test "day04_part2" {}
+test "day04_part1" {
+    const answer = try part1(test_allocator, test_input);
+    try std.testing.expectEqual(18, answer);
+}
+
+test "day04_part2" {
+    const answer = try part2(test_allocator, test_input);
+    try std.testing.expectEqual(9, answer);
+}
--- src/utils.zig
+++ src/utils.zig
@@ -33,6 +33,72 @@ pub fn read_num(seq: []const u8) !struct { usize, ?u32 } {
     return .{ ind, num };
 }
 
+pub fn Matrix(comptime T: type) type {
+    return struct {
+        items: []T,
+        n_rows: usize,
+        n_cols: usize,
+        allocator: Allocator,
+
+        const Self = @This();
+        const InitError = error{
+            UnevenColumns,
+        };
+
+        pub fn init(allocator: Allocator, n_rows: usize, n_cols: usize) !Self {
+            return .{
+                .items = try allocator.alloc(T, n_rows * n_cols),
+                .n_rows = n_rows,
+                .n_cols = n_cols,
+                .allocator = allocator,
+            };
+        }
+
+        pub fn deinit(self: Self) void {
+            self.allocator.free(self.items);
+        }
+
+        pub fn initFromString(allocator: Allocator, string: []const u8) !Self {
+            var n_rows: usize = 0;
+            var n_cols: usize = 0;
+            var items = std.ArrayList(T).init(allocator);
+            var line_iter = std.mem.tokenizeScalar(u8, string, '\n');
+            while (line_iter.next()) |line| {
+                if (line.len > n_cols) {
+                    if (n_cols == 0) {
+                        n_cols = line.len;
+                    } else {
+                        return InitError.UnevenColumns;
+                    }
+                }
+                try items.appendSlice(line);
+                n_rows += 1;
+            }
+            return .{
+                .items = try items.toOwnedSlice(),
+                .n_rows = n_rows,
+                .n_cols = n_cols,
+                .allocator = allocator,
+            };
+        }
+
+        pub fn get(self: Self, row: usize, col: usize) T {
+            return self.items[row * self.n_cols + col];
+        }
+
+        pub fn debugPrint(self: Self) !void {
+            for (0..self.n_rows) |i| {
+                for (0..self.n_cols) |j| {
+                    const value = self.get(i, j);
+                    try std.fmt.formatInt(value, 10, std.fmt.Case.lower, .{}, stderr);
+                    _ = try stderr.write(" ");
+                }
+                _ = try stderr.write("\n");
+            }
+        }
+    };
+}
+
 test "read_num" {
     const seq = "234sd4";
     try std.testing.expectEqual(.{ 3, 234 }, try read_num(seq));
